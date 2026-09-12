0914c580829ff3b96abbed0f08d484c7c04249f3
Shane O'Sullivan <shaneosullivan4@gmail.com>
2026-09-12 22:27:55 +0100

    vpc module

--- main.tf
+++ main.tf
@@ -18,6 +18,22 @@ data "aws_vpc" "default" {
   default = true
 } 
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -34,7 +50,7 @@ module "blog_sg" {
   version = "6.0.0"
 
   name   = "blog_new"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules = {
     http = {
